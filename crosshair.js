5acccac64786b8554a2c4712a5fb20cbcc5ddf24
Randall Hand <randall.hand@gmail.com>
2024-07-19 22:23:11 -0400

    Bugfixes

--- crosshair.js
+++ crosshair.js
@@ -4,15 +4,14 @@ export function autocomplete(data, args) {
 }
 /** @param {NS} ns */
 export async function main(ns) {
-  const target= ns.args[0]
+  const targets = ns.args
   var ramScript = Math.max(ns.getScriptRam("loop_hack.js"),
                             ns.getScriptRam("loop_weaken.js"), 
                             ns.getScriptRam("loop_grow.js"))
+  ns.tprintf("Cross targets: %s", targets)
 
-  var cmdArgs = [ target,
-    "--maxmoney", ns.getServerMaxMoney(target),
-    "--minsec", ns.getServerMinSecurityLevel(target)]
 
+  var targetId = 0
   for(const S of getServerList(ns).concat(ns.getPurchasedServers())) {
     // copy the scripts we need
     ns.scp("reh.js", S)
@@ -28,15 +27,22 @@ export async function main(ns) {
     if(!ns.hasRootAccess(S))
       continue
 
+    var target = targets[targetId]
+    var cmdArgs = [
+      "--maxmoney", ns.getServerMaxMoney(target),
+      "--minsec", ns.getServerMinSecurityLevel(target)]
+    targetId = (targetId + 1) % targets.length
     var tWeaken = Math.max(1, Math.floor((ram / ramScript) * 0.15))
     var tHack   = Math.max(1, Math.floor((ram / ramScript) * 0.10))
     var tGrow   = Math.max(1, Math.floor((ram - (tWeaken + tHack)*ramScript) 
                 / ramScript))
+    ns.tprintf("-> %s Crosshair target: %s [H:%i G:%i W:%i]", 
+          S, target, tHack, tGrow, tWeaken)
 
     ns.killall(S);
     await ns.sleep(250);
-    ns.exec("loop_hack.js",   S, tHack, ...cmdArgs);
-    ns.exec("loop_weaken.js", S, tWeaken, ...cmdArgs)
-    ns.exec("loop_grow.js",   S, tGrow, ...cmdArgs)
+    ns.exec("loop_hack.js",   S, tHack, target, ...cmdArgs);
+    ns.exec("loop_weaken.js", S, tWeaken, target, ...cmdArgs)
+    ns.exec("loop_grow.js",   S, tGrow, target, ...cmdArgs)
   }
 }
