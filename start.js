cf7236f958f01f19e75038def07144eb77597f03
Randall Hand <randall.hand@gmail.com>
2024-07-21 08:43:18 -0400

    Cleaned up

--- start.js
+++ start.js
@@ -19,14 +19,15 @@ export async function main(ns) {
     if(ns.scriptRunning("build-farm.js", "home") == false)
       ns.exec("build-farm.js", "home")
     ns.tprintf("starting listall.js")
-    if(ns.scriptRunning("listall.js", "home") == false)
-      ns.exec("listall.js", "home", 1, "--loop")
+    if(ns.scriptRunning("listtiny.js", "home") == false)
+      ns.exec("listtiny.js", "home", 1, "--loop")
   }
   var localThreadCount = Math.max(4, 
           Math.floor((ns.getServerMaxRam("home") * .75) / 2))
   ns.tprintf("localThread safety = %i" , localThreadCount)
   // At the beginning of the game, not much we can do but hack n00dles
   if (ns.getHackingLevel() < 10) {
+    await ns.sleep(5000)
     ns.exec("blast.js", "home",1, "n00dles", localThreadCount)
     while (ns.getHackingLevel() < 10)
       await ns.sleep(1000);
@@ -50,30 +51,38 @@ export async function main(ns) {
     while (ns.getHackingLevel() < 300) {
       await ns.sleep(1000);
       var newServers = false
-      var pservRam = ns.getPurchasedServers().reduce(
-        (a,b) => a + ns.getServerMaxRam(b), 0    )
-      if (pservRam > ramPServers) {
-        newServers = true
-        ramPServers = pservRam
-      }
-      var curnumNPCServers = getServerList(ns).reduce(
-        (a, S) => a+ (ns.hasRootAccess(S) ? 1 : 0),0)
-      if(curnumNPCServers > numNPCServers) {
-        newServers = true
-        numNPCServers = curnumNPCServers
-      }
-      if (newServers) {
-        ns.exec("blast.js", "home",1, "joesguns", "EXTEND", localThreadCount)
+      if (!ns.scriptRunning("blast.js", "home")) {
+        var pservRam = ns.getPurchasedServers().reduce(
+          (a,b) => a + ns.getServerMaxRam(b), 0    )
+        if (pservRam > ramPServers) {
+          newServers = true
+          ramPServers = pservRam
+        }
+        var curnumNPCServers = getServerList(ns).reduce(
+          (a, S) => a+ (ns.hasRootAccess(S) ? 1 : 0),0)
+        if(curnumNPCServers > numNPCServers) {
+          newServers = true
+          numNPCServers = curnumNPCServers
+        }
+        if (newServers) {
+          ns.exec("blast.js", "home",1, "joesguns", "EXTEND", localThreadCount)
+        }
       }
     }
-    ns.exec("global-cleanup.js", "home", 1, "--loop")
-    await ns.sleep(1000)
   }
 
   // Once we hit level 300, we can move on to 
   // joesguns & phantasy
 
+
+  // Same as before, but also look at 
+  // upgrading our purchased servers
+  var pservLevel =0
   if(ns.getHackingLevel() < 1000) {
+    while(!ns.hasRootAccess("phantasy"))
+      await ns.sleep(1000)
+    ns.exec("global-cleanup.js", "home", 1, "--loop")
+    await ns.sleep(1000)
     ns.exec("blast.js", "home",1, "joesguns", "phantasy", localThreadCount)
     var ramPServers = ns.getPurchasedServers().reduce(
         (a,b) => a + ns.getServerMaxRam(b), 0  )
@@ -82,26 +91,48 @@ export async function main(ns) {
     while (ns.getHackingLevel() < 1000) {
       await ns.sleep(1000);
       var newServers = false
-      var pservRam = ns.getPurchasedServers().reduce(
-        (a,b) => a + ns.getServerMaxRam(b), 0    )
-      if (pservRam > ramPServers) {
-        newServers = true
-        ramPServers = pservRam
+      if(!ns.scriptRunning("blast.js", "home")) {
+        var pservRam = ns.getPurchasedServers().reduce(
+          (a,b) => a + ns.getServerMaxRam(b), 0    )
+        if (pservRam > ramPServers) {
+          newServers = true
+          ramPServers = pservRam
+        }
+        var curnumNPCServers = getServerList(ns).reduce(
+          (a, S) => a+ (ns.hasRootAccess(S) ? 1 : 0),0)
+        if(curnumNPCServers > numNPCServers) {
+          newServers = true
+          numNPCServers = curnumNPCServers
+        }
+        if (newServers) {
+          ns.exec("blast.js", "home",1, "joesguns", "phantasy", "EXTEND", localThreadCount)
+        }
       }
-      var curnumNPCServers = getServerList(ns).reduce(
-        (a, S) => a+ (ns.hasRootAccess(S) ? 1 : 0),0)
-      if(curnumNPCServers > numNPCServers) {
-        newServers = true
-        numNPCServers = curnumNPCServers
+      if (!ns.scriptRunning("build-farm.js", "home")) {
+        if ((pservLevel == 0) && (ns.getServerMoneyAvailable("home") > 25000000)) {
+          pservLevel=1
+          ns.exec("build-farm.js", "home", 1, "--upgrade", 32)
+        } else if ((pservLevel == 1) && (ns.getServerMoneyAvailable("home") > 500000000)) {
+          pservLevel=2
+          ns.exec("build-farm.js", "home", 1, "--upgrade", 512)
+        } else if ((pservLevel == 2) && (ns.getServerMoneyAvailable("home") > 1000000000)) {
+          pservLevel=3
+          ns.exec("build-farm.js", "home", 1, "--upgrade", 1024)
+        } else if ((pservLevel == 3) && (ns.getServerMoneyAvailable("home") > 4000000000)) {
+          pservLevel=4
+          ns.exec("build-farm.js", "home", 1, "--upgrade", 4096)
+        }
       }
-      if (newServers) {
-        ns.exec("blast.js", "home",1, "joesguns", "phantasy", "EXTEND", localThreadCount)
-      }
+
     }
     ns.exec("global-cleanup.js", "home", 1, "--loop")
     await ns.sleep(1000)
   }
-  ns.exec("blast.js", "home",1, "joesguns", "phantasy", "the-hub", "4")
+  while(!ns.hasRootAccess("the-hub"))
+    await ns.sleep(1000)
+  ns.exec("global-cleanup.js", "home", 1, "--loop")
+  await ns.sleep(1000)
+  ns.exec("blast.js", "home",1, "joesguns", "phantasy", "the-hub", localThreadCount)
   // Once we hit level 1000, just let 
   // these 3run open with a blast.. Anything beyond here
   //  I'llmanage by hand for now
