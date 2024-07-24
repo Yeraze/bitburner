54a455e3543399b937c8ada7cc7f5cc4eb781333
Randall Hand <randall.hand@gmail.com>
2024-07-24 10:34:31 -0400

    Clean up a spammy toast

--- servers/home/singularity-factionjoin.js
+++ servers/home/singularity-factionjoin.js
@@ -2,8 +2,8 @@ import {rehprintf} from 'reh.js'
 /** @param {NS} ns */
 export async function main(ns) {
     const faction = ns.args[0]
-    ns.singularity.joinFaction(faction)
-    ns.toast(ns.sprintf("Joined faction %s", faction), "success", null)
+    if(ns.singularity.joinFaction(faction))
+        ns.toast(ns.sprintf("Joined faction %s", faction), "success", null)
     // If we have the NMI we can background the hacking
     var focus = ns.singularity.getOwnedAugmentations(false).indexOf("Neuroreceptor Management Implant") == -1
     ns.singularity.workForFaction(faction, "hacking", focus)
