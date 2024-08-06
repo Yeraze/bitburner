cf5aceedaae553d50fcca14b6f04a9a54f089209
Randall Hand <randall.hand@gmail.com>
2024-08-06 07:22:39 -0400

    Improvements to Stanek

--- servers/home/singularity-stanek.js
+++ servers/home/singularity-stanek.js
@@ -1,16 +1,19 @@
-import {rehprintf, qualifyAugment} from 'reh.js'
+import {rehprintf, qualifyAugment, parsearg} from 'reh.js'
 import * as CONST from 'reh-constants.js'
 import * as db from 'database.js'
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.tail()
+    //ns.tail()
     if(!ns.stanek.acceptGift()) {
         db.dbLogf(ns, "ERROR: Unable to accept Stanek's gift")
         return
     }
-    while(true) {
+    var cycles = 100
+    cycles = parsearg(ns, "--cycles", 25)
+    db.dbLogf(ns, "STANEK: Charging %i cycles", cycles)
+    for(var counter = 0; counter < cycles; counter++) {
         await ns.sleep(10)
-        ns.printf("Beginning charge loop...")
+        ns.printf("Charge cycle: %i of %i", counter, cycles)
         for(var fragment of ns.stanek.activeFragments()) {
             ns.printf("-> [%i] %i,%i - %i", fragment.id, fragment.x, fragment.y, fragment.highestCharge)
             try {
@@ -19,7 +22,7 @@ export async function main(ns) {
                 ns.printf("ERROR: %s", error)
             }
         }
-
     }
+    db.dbLogf(ns, "STANEK: Done with %i cycles", cycles)
 
 }
