4f1c63b55dec6891814f4a3a44fefe66bc50a9a2
Randall Hand <randall.hand@gmail.com>
2024-08-11 10:19:07 -0500

    Added logging

--- servers/home/singularity-home.js
+++ servers/home/singularity-home.js
@@ -5,18 +5,20 @@ export async function main(ns) {
   var ramCost = ns.singularity.getUpgradeHomeRamCost()
   if (ns.getServerMoneyAvailable("home") > ramCost) {
     if (await doCommand(ns, "ns.singularity.upgradeHomeRam()")) {
-      rehprintf(ns, "Upgraded home RAM")
-      ns.toast(ns.sprintf("Upgrading home RAM: $%s", ns.formatNumber(ramCost)),
+      //rehprintf(ns, "Upgraded home RAM")
+      ns.toast(ns.sprintf("Upgraded home RAM: $%s", ns.formatNumber(ramCost)),
           "success", null)  
+      db.dbLogf(ns, "Upgraded home RAM: %s", ns.formatNumber(ramCost))
     }
   }
 
   var coreCost = ns.singularity.getUpgradeHomeCoresCost()
   if (ns.getServerMoneyAvailable("home") > coreCost) {
     if(await doCommand(ns, "ns.singularity.upgradeHomeCores()")) {
-      rehprintf(ns, "Upgraded home CORES")
-      ns.toast(ns.sprintf("Upgrading home CORES: $%s", ns.formatNumber(coreCost)),
+      //rehprintf(ns, "Upgraded home CORES")
+      ns.toast(ns.sprintf("Upgraded home CORES: $%s", ns.formatNumber(coreCost)),
           "success", null)      
+      db.dbLogf(ns, "Upgraded home CORES: $%s", ns.formatNumber(coreCost))
     }
   }
 
