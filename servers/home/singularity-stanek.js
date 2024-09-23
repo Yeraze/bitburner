bcdf3b3433abdd65c570d711399135b1ede6f062
Randall Hand <randall.hand@gmail.com>
2024-09-23 17:00:12 -0500

    Lots of output improvement to Stanek charge

--- servers/home/singularity-stanek.js
+++ servers/home/singularity-stanek.js
@@ -3,33 +3,46 @@ import * as CONST from 'reh-constants.js'
 import * as db from 'database.js'
 /** @param {NS} ns */
 export async function main(ns) {
-    //ns.tail()
     ns.disableLog('ALL')
     if(ns.getResetInfo().ownedAugs.has("Stanek's Gift - Genesis") == false) {
         db.dbLogf(ns, "STANEK: Gift not found, exiting")
         return
     }
+    ns.printf("Preparing to charge Stanek")
     var cycles = 100
     cycles = parsearg(ns, "--cycles", 25)
+    ns.printf(" --> %i cycles", cycles)
     db.dbLogf(ns, "STANEK: Charging %i cycles", cycles)
     var trickle = ns.args.includes('--trickle')
+    if (!trickle) { 
+      ns.tail()
+      ns.moveTail(400,300)
+      ns.resizeTail(510, 100)
+    }
     var fragments = []
     for(var fragment of ns.stanek.activeFragments()) {
         try {
             await ns.stanek.chargeFragment(fragment.x, fragment.y)
             fragments.push(fragment)
         } catch (error) {
-            ns.printf("ERROR: %s", error)
+            //ns.printf("ERROR: %s", error)
         }
     }
 
-    for(var counter = 0; counter < cycles; counter++) {
-        ns.clearLog()
-        ns.printf("Charge cycle: %i of %i", counter, cycles)
+    const steps = fragments.length * cycles
+    var curStep = 0
 
-        await ns.sleep(10)
+    for(var counter = 0; counter < cycles; counter++) {
         for(var fragment of fragments) {
-            ns.printf("-> [%i] %i,%i - %i", fragment.id, fragment.x, fragment.y, fragment.highestCharge)
+            ns.clearLog()
+            curStep += 1
+            
+            let length = 50
+            let progress = Math.floor(length * curStep / steps)
+            let sProgress = new Array(progress+1).join( '#' );
+            ns.printf("Charging %i Stanek fragments", fragments.length)
+            ns.printf("[%-50s]", sProgress)
+            //ns.printf("-> [%i] %i,%i - %i", fragment.id, fragment.x, fragment.y, fragment.highestCharge)
             var cmd = `await ns.stanek.chargeFragment(${fragment.x}, ${fragment.y})`
             if (trickle)
                 await doCommand(ns, cmd)
@@ -38,5 +51,5 @@ export async function main(ns) {
         }
     }
     db.dbLogf(ns, "STANEK: Done with %i cycles", cycles)
-
+    ns.closeTail()
 }
